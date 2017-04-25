5cac04c1f9731e5e2c7b6e4557c63a402e7e2f3f
Zhou <zhijun.zhou@hpe.com>
2017-04-25 11:08:54 +0800

    split the event listener plugin logic processing

--- jira-node-api/routes/issues.js
+++ jira-node-api/routes/issues.js
@@ -72,13 +72,13 @@ router.get('/sla/:issueId', function (req, res, next) {
             var endDate = myIssue.fields[global.customFields.endIssueDate.id]; //'2017-04-19T09:34:11.748+0000'; // todo
             var startDate = myIssue.fields.created; //'2017-04-12T09:34:11.748+0000'; // todo		
             var priority = myIssue.fields.priority.name;
-            var serveHours =  0;
+            var serveHours = 0;
 
-            if(priority === "P3") {
+            if (priority === "P3") {
                 serveHours = slaHelper.getServeHours(startDate, endDate, watchers, global.standard.noWorkHours[0], global.standard.noWorkHours[1]);
             } else {
                 serveHours = slaHelper.getServeHours(startDate, endDate, watchers, global.standard.workHours[0], global.standard.workHours[1]);
-            }            
+            }
 
             var sla = 0;
             var zone = myIssue.fields[global.customFields.zone.id] || "1"; // todo
@@ -127,113 +127,12 @@ router.put('/:issueId', function (req, res, next) {
 })
 
 router.post('/plugin/update/:issueId', function (req, res, next) {
-    res.json({});
-    ctrl.getIssueById(req.params.issueId + '?expand=changelog').then(function (issue) {
-        var myIssue = typeof issue === "string" ? JSON.parse(issue) : issue;
-        var eventType = myIssue.fields[global.customFields.eventType.id];
-        var storeArea = myIssue.fields[global.customFields.storeArea.id];
-        var storeNo = myIssue.fields[global.customFields.storeNo.id];
-
-        var histories = myIssue.changelog.histories;
-
-        if ((Array.isArray(histories) && histories.length === 0)) {
-            var defaultEventType = "";
-            if (eventType == null) {
-                defaultEventType = "甲服务商项目组";
-            } else {
-                defaultEventType = eventType.value;
-            }
-            if (defaultEventType !== "") {
-                var requestUrl = config.ddtalk + "?districtname=" + storeArea + "&diningname=" + storeNo + "&vendorname=" + defaultEventType;
-
-                ctrl.extService(encodeURI(requestUrl)).then(function (result) {
-                    var result = typeof result === "string" ? JSON.parse(result) : result;
-                    if (result && result.data && result.data.serviceDeskName) {
-
-
-                        var serveStationId = global.customFields.serveStation.id;
-                        var serveStation = result.data.serviceDeskName;
-
-                        var body = {
-                            "fields": {}				
-                        };
-                        body["fields"][serveStationId] = serveStation;
-
-                        slaHelper.getZone(serveStation, storeNo, function(err, myZone) {
-                            body["fields"][global.customFields.zone.id] = myZone + "";
-                            // update serve station
-                            ctrl.updateIssue(req.params.issueId, body).then(function () {
-                                var vendor = defaultEventType.indexOf("乙") !== -1 ? "Vendor2" : "Vendor1";
-                                // update assignee
-                                ctrl.updateAssignee(req.params.issueId, vendor);
-                            })
-                        })
-							
-                        
-                    }
-                }).catch(function (err) {
-                    output(err);
-                });
-            }
-        } else if ((myIssue.fields.status.name == "In Progress")) {
-			
-			var defaultEventType = "";
-            if (eventType == null) {
-                defaultEventType = "甲服务商项目组";
-            } else {
-                defaultEventType = eventType.value;
-            }
-			if (defaultEventType !== "") {
-				var requestUrl = config.ddtalk + "?districtname=" + storeArea + "&diningname=" + storeNo + "&vendorname=" + defaultEventType;
-
-                ctrl.extService(encodeURI(requestUrl)).then(function (result) {
-					var result = typeof result === "string" ? JSON.parse(result) : result;
-					// call itsm robot
-					var serveStation = myIssue.fields[global.customFields.serveStation.id];
-					if (result && result.data && result.data.serviceDeskName) {
-						var serveStationId = global.customFields.serveStation.id;
-                        var serveStation = result.data.serviceDeskName;
-						if (serveStation) {
-							// ctrl.extService(encodeURI(config.ddRobot + "?issuekey=" + myIssue.key + "&deskname=" + serveStation));
-							slaHelper.getRobotToken(function(err, rows){
-								var token = '';
-								if(err) {return;}
-								for(var k = 0;k < rows.length;k++) {
-									if(serveStation === rows[k]['service_desk_name']) {
-										token = rows[k]['custom_robot_token'];
-										break;
-									}
-								}
-								var link = {
-									"msgtype": "link", 
-									"link": {
-										"text": myIssue.key, 
-										"title": myIssue.key, 
-										"picUrl": "", 
-										"messageUrl": "http://itsmpoc6341.cloudapp.net:7777/pages/requestCloseCase.html?key=" + myIssue.key
-									}
-								}
-								ctrl.sendLink(config.itsmRobot + token, link);
-							})
-						}
-					}
-				});
-			}
-        } else {
-
-        }
-        // comment this line in prod environment
-        // res.json({});
-    }).catch(function (err) {
-        // res.json(err);
-        output(err);
-    })
 })
 
 router.post('/end/:issueId', function (req, res, next) {
     var issueId = req.params.issueId;
     var body = req.body;
-	
+
     ctrl.endIssue(issueId, body).then(function (result) {
         res.json(result);
     }).catch(function (err) {
@@ -241,6 +140,100 @@ router.post('/end/:issueId', function (req, res, next) {
         output(err);
     })
 })
+
+router.post('/plugin/update/:issueId', function (req, res, next) {
+    ctrl.getIssueById(req.params.issueId + '?expand=changelog').then(function (issue) {
+        var myIssue = typeof issue === "string" ? JSON.parse(issue) : issue;
+        var eventType = myIssue.fields[global.customFields.eventType.id];
+        var storeArea = myIssue.fields[global.customFields.storeArea.id];
+        var storeNo = myIssue.fields[global.customFields.storeNo.id];
+        var histories = myIssue.changelog.histories;
+
+        var defaultEventType = "";
+        if (eventType == null) {
+            defaultEventType = "甲服务商项目组";
+        } else {
+            defaultEventType = eventType.value;
+        }
+        if (defaultEventType !== "") {
+            var requestUrl = config.ddtalk + "?districtname=" + storeArea + "&diningname=" + storeNo + "&vendorname=" + defaultEventType;
+
+            ctrl.extService(encodeURI(requestUrl)).then(function (result) {
+                var result = typeof result === "string" ? JSON.parse(result) : result;
+                if (result && result.data && result.data.serviceDeskName) {
+                    var serveStationId = global.customFields.serveStation.id;
+                    var serveStation = result.data.serviceDeskName;
+
+                    var body = {
+                        "fields": {}
+                    };
+                    body["fields"][serveStationId] = serveStation;
+
+                    slaHelper.getZone(serveStation, storeNo, function (err, myZone) {
+                        body["fields"][global.customFields.zone.id] = myZone + "";
+                        // update serve station
+                        ctrl.updateIssue(req.params.issueId, body).then(function () {
+                            var vendor = defaultEventType.indexOf("乙") !== -1 ? "Vendor2" : "Vendor1";
+                            // update assignee
+                            ctrl.updateAssignee(req.params.issueId, vendor);
+                        })
+                    })
+                }
+            }).catch(function (err) {
+                output(err);
+            });
+        }
+    })
+});
+
+router.post('/plugin/robot/:issueId', function (req, res, next) {
+    ctrl.getIssueById(req.params.issueId + '?expand=changelog').then(function (issue) {
+        var myIssue = typeof issue === "string" ? JSON.parse(issue) : issue;
+
+        var defaultEventType = "";
+        if (eventType == null) {
+            defaultEventType = "甲服务商项目组";
+        } else {
+            defaultEventType = eventType.value;
+        }
+        if (defaultEventType !== "") {
+            var requestUrl = config.ddtalk + "?districtname=" + storeArea + "&diningname=" + storeNo + "&vendorname=" + defaultEventType;
+
+            ctrl.extService(encodeURI(requestUrl)).then(function (result) {
+                var result = typeof result === "string" ? JSON.parse(result) : result;
+                // call itsm robot
+                var serveStation = myIssue.fields[global.customFields.serveStation.id];
+                if (result && result.data && result.data.serviceDeskName) {
+                    var serveStationId = global.customFields.serveStation.id;
+                    var serveStation = result.data.serviceDeskName;
+                    if (serveStation) {
+                        // ctrl.extService(encodeURI(config.ddRobot + "?issuekey=" + myIssue.key + "&deskname=" + serveStation));
+                        slaHelper.getRobotToken(function (err, rows) {
+                            var token = '';
+                            if (err) { return; }
+                            for (var k = 0; k < rows.length; k++) {
+                                if (serveStation === rows[k]['service_desk_name']) {
+                                    token = rows[k]['custom_robot_token'];
+                                    break;
+                                }
+                            }
+                            var link = {
+                                "msgtype": "link",
+                                "link": {
+                                    "text": myIssue.key,
+                                    "title": myIssue.key,
+                                    "picUrl": "",
+                                    "messageUrl": "http://itsmpoc6341.cloudapp.net:7777/pages/requestCloseCase.html?key=" + myIssue.key
+                                }
+                            }
+                            ctrl.sendLink(config.itsmRobot + token, link);
+                        })
+                    }
+                }
+            });
+        }
+    })
+});
 
 
 module.exports = router;
